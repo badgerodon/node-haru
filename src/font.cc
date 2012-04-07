e213027935a46c0f11e1f35e85574bb0553e7688
Caleb Doxsey <cdoxsey@trada.com>
2012-04-07 14:53:43 -0600

    latest

--- src/font.cc
+++ src/font.cc
@@ -2,6 +2,9 @@
 
 using namespace Haru;
 
+Font::Font(): ObjectWrap() {}
+Font::~Font() {}
+
 // Page
 Persistent<FunctionTemplate> Font::constructor;
 void Font::Initialize(Handle<Object> target) {
@@ -12,14 +15,31 @@ void Font::Initialize(Handle<Object> target) {
   constructor->SetClassName(String::NewSymbol("Font"));
 
   Local<ObjectTemplate> proto = constructor->PrototypeTemplate();
+  NODE_SET_PROTOTYPE_METHOD(constructor, "getAscent", GetAscent);
+  NODE_SET_PROTOTYPE_METHOD(constructor, "getBBox", GetBBox);
+  NODE_SET_PROTOTYPE_METHOD(constructor, "getCapHeight", GetCapHeight);
+  NODE_SET_PROTOTYPE_METHOD(constructor, "getDescent", GetDescent);
+  NODE_SET_PROTOTYPE_METHOD(constructor, "getFontName", GetFontName);
+  NODE_SET_PROTOTYPE_METHOD(constructor, "getUnicodeWidth", GetUnicodeWidth);
+  NODE_SET_PROTOTYPE_METHOD(constructor, "getXHeight", GetXHeight);
+  NODE_SET_PROTOTYPE_METHOD(constructor, "measureText", MeasureText);
+  NODE_SET_PROTOTYPE_METHOD(constructor, "textWidth", TextWidth);
 
   target->Set(String::NewSymbol("Font"), constructor->GetFunction());
 }
+
+Handle<Value> Font::GetAscent(const Arguments &args) { HandleScope scope; raise("not implemented"); }
+Handle<Value> Font::GetBBox(const Arguments &args) { HandleScope scope; raise("not implemented"); }
+Handle<Value> Font::GetCapHeight(const Arguments &args) { HandleScope scope; raise("not implemented"); }
+Handle<Value> Font::GetDescent(const Arguments &args) { HandleScope scope; raise("not implemented"); }
+Handle<Value> Font::GetFontName(const Arguments &args) { HandleScope scope; raise("not implemented"); }
+Handle<Value> Font::GetUnicodeWidth(const Arguments &args) { HandleScope scope; raise("not implemented"); }
+Handle<Value> Font::GetXHeight(const Arguments &args) { HandleScope scope; raise("not implemented"); }
+Handle<Value> Font::MeasureText(const Arguments &args) { HandleScope scope; raise("not implemented"); }
 Handle<Value> Font::New(const Arguments &args) {
   HandleScope scope;
   Font* font = new Font();
   font->Wrap(args.This());
   return args.This();
 }
-Font::Font(): ObjectWrap() {}
-Font::~Font() {}
+Handle<Value> Font::TextWidth(const Arguments &args) { HandleScope scope; raise("not implemented"); }
--- src/font.h
+++ src/font.h
@@ -9,7 +9,16 @@ namespace Haru {
       static Persistent<FunctionTemplate> constructor;
       static void Initialize(Handle<Object> target);
       // JS methods
+      static Handle<Value> GetAscent(const Arguments &args);
+      static Handle<Value> GetBBox(const Arguments &args);
+      static Handle<Value> GetCapHeight(const Arguments &args);
+      static Handle<Value> GetDescent(const Arguments &args);
+      static Handle<Value> GetFontName(const Arguments &args);
+      static Handle<Value> GetUnicodeWidth(const Arguments &args);
+      static Handle<Value> GetXHeight(const Arguments &args);
+      static Handle<Value> MeasureText(const Arguments &args);
       static Handle<Value> New(const Arguments &args);
+      static Handle<Value> TextWidth(const Arguments &args);
 
       HPDF_Font font;
 
